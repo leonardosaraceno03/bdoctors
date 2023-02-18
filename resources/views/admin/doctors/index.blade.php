026a8ab3adb1ffc136708c275323800845f68879
marcoaufiero <marcoaufiero96@gmail.com>
2023-02-18 12:49:54 +0100

    div container added to views doctors

--- resources/views/admin/doctors/index.blade.php
+++ resources/views/admin/doctors/index.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.app')
 @section('content')
-@foreach ($doctors as $doctor)
-    <div>
-        <h2>{{ $doctor['name'] }} {{ $doctor['surname'] }}</h2>
-        <p>Address: {{ $doctor['address'] }}</p>
-        <p>Telephone: {{ $doctor['telephone'] }}</p>
-        <p>Performance: {{ $doctor['performance'] }}</p>
-        <p>Description: {{ $doctor['description'] }}</p>
-        <p>Specialization: {{ $doctor['specializations'] }}</p>
-    </div>
-    <a href="{{route('admin.doctors.edit', $doctor['id'])}}">
-        <button class="btn btn-dark text-info">Edit</button>
-    </a>
-@endforeach
+<div class="container mt-5">
+    @foreach ($doctors as $doctor)
+        <div>
+            <h2>{{ $doctor['name'] }} {{ $doctor['surname'] }}</h2>
+            <p>Address: {{ $doctor['address'] }}</p>
+            <p>Telephone: {{ $doctor['telephone'] }}</p>
+            <p>Performance: {{ $doctor['performance'] }}</p>
+            <p>Description: {{ $doctor['description'] }}</p>
+            <p>Specialization: {{ $doctor['specializations'] }}</p>
+        </div>
+        <a href="{{route('admin.doctors.edit', $doctor['id'])}}">
+            <button class="btn btn-dark text-info">Edit</button>
+        </a>
+    @endforeach
+</div>
+
 @endsection
