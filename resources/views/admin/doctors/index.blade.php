ff8d874d9d0f141b004a8836dc1603c15b15efa1
marcoaufiero <marcoaufiero96@gmail.com>
2023-02-20 10:43:15 +0100

    restyle back-end crud pages

--- resources/views/admin/doctors/index.blade.php
+++ resources/views/admin/doctors/index.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.app')
 @section('content')
 <div class="container mt-5">
+    <h1 class="text-primary">Dashboard</h1>
     @foreach ($doctors as $doctor)
         <div>
-            <h2>{{ $doctor['name'] }} {{ $doctor['surname'] }}</h2>
+            <h2>Benvenuto, {{ $doctor['name'] }} {{ $doctor['surname'] }}</h2>
             <p>Address: {{ $doctor['address'] }}</p>
-            <p>Telephone: {{ $doctor['telephone'] }}</p>
-            <p>Performance: {{ $doctor['performance'] }}</p>
-            <p>Description: {{ $doctor['description'] }}</p>
             <p>Specialization: {{ $doctor['specializations'] }}</p>
         </div>
         <a href="{{route('admin.doctors.edit', $doctor['id'])}}">
