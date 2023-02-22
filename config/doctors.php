f0f8e50bd0de4048990d0c871b764c40b0827c48
marcoaufiero <marcoaufiero96@gmail.com>
2023-02-22 16:56:56 +0100

    seeders ampiati

--- config/doctors.php
+++ config/doctors.php
@@ -8,8 +8,7 @@ return [
         'performance' => 'Eccellente',
         'description' => 'Lorem ipsum dolor sit amet...',
         'visibility' => true,
-        'user_id' => 1, // ID dell'utente associato
-        
+        'user_id' => 1, // ID dell'utente associato  
     ],
     [
         'address' => 'Via Milano, 456',
@@ -20,7 +19,87 @@ return [
         'description' => 'Lorem ipsum dolor sit amet...',
         'visibility' => true,
         'user_id' => 2, // ID dell'utente associato
-        
     ],
-    // e così via per gli altri dottori
+    [
+        'address' => 'Via Milano, 456',
+        'cv' => 'Lorem ipsum dolor sit amet...',
+        'avatar' => 'default-avatar.jpg',
+        'telephone' => '9876543210',
+        'performance' => 'Ottimo',
+        'description' => 'Lorem ipsum dolor sit amet...',
+        'visibility' => true,
+        'user_id' => 3, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Dante, 234',
+        'cv' => 'Lorem ipsum dolor sit amet...',
+        'avatar' => 'default-avatar.jpg',
+        'telephone' => '3334445555',
+        'performance' => 'Molto buono',
+        'description' => 'Lorem ipsum dolor sit amet...',
+        'visibility' => true,
+        'user_id' => 4, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Piazza del Popolo, 567',
+        'cv' => 'Lorem ipsum dolor sit amet...',
+        'avatar' => 'default-avatar.jpg',
+        'telephone' => '9998887777',
+        'performance' => 'Eccellente',
+        'description' => 'Lorem ipsum dolor sit amet...',
+        'visibility' => true,
+        'user_id' => 5, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Garibaldi, 890',
+        'cv' => 'Lorem ipsum dolor sit amet...',
+        'avatar' => 'default-avatar.jpg',
+        'telephone' => '1112223333',
+        'performance' => 'Discreto',
+        'description' => 'Lorem ipsum dolor sit amet...',
+        'visibility' => true,
+        'user_id' => 6, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via delle Rose, 123',
+        'cv' => 'Lorem ipsum dolor sit amet...',
+        'avatar' => 'default-avatar.jpg',
+        'telephone' => '4445556666',
+        'performance' => 'Molto buono',
+        'description' => 'Lorem ipsum dolor sit amet...',
+        'visibility' => true,
+        'user_id' => 7, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via dei Mille, 456',
+        'cv' => 'Lorem ipsum dolor sit amet...',
+        'avatar' => 'default-avatar.jpg',
+        'telephone' => '7778889999',
+        'performance' => 'Ottimo',
+        'description' => 'Lorem ipsum dolor sit amet...',
+        'visibility' => true,
+        'user_id' => 8, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via dei Tigli, 234',
+        'cv' => 'Lorem ipsum dolor sit amet...',
+        'avatar' => 'default-avatar.jpg',
+        'telephone' => '4443332222',
+        'performance' => 'Eccellente',
+        'description' => 'Lorem ipsum dolor sit amet...',
+        'visibility' => true,
+        'user_id' => 9, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via del Corso, 567',
+        'cv' => 'Lorem ipsum dolor sit amet...',
+        'avatar' => 'default-avatar.jpg',
+        'telephone' => '8887776666',
+        'performance' => 'Molto buono',
+        'description' => 'Lorem ipsum dolor sit amet...',
+        'visibility' => true,
+        'user_id' => 10, // ID dell'utente associato
+    ],
+    
+      // e così via per gli altri dottori
 ];
--- config/users.php
+++ config/users.php
@@ -14,6 +14,54 @@ return [
         'email' => 'giuseppe.verdi@example.com',
         'password' => '12345678',
     ],
+    [
+        'name' => 'Marco',
+        'surname' => 'Bianchi',
+        'email' => 'marco.bianchi@example.com',
+        'password' => '12345678',
+    ],
+    [
+        'name' => 'Luca',
+        'surname' => 'Pesci',
+        'email' => 'luca.pesci@example.com',
+        'password' => '12345678',
+    ],
+    [
+        'name' => 'Laura',
+        'surname' => 'Conti',
+        'email' => 'laura.conti@example.com',
+        'password' => '12345678',
+    ],
+    [
+        'name' => 'Paolo',
+        'surname' => 'Monti',
+        'email' => 'paolo.monti@example.com',
+        'password' => '12345678',
+    ],
+    [
+        'name' => 'Francesca',
+        'surname' => 'Resti',
+        'email' => 'francesca.resti@example.com',
+        'password' => '12345678',
+    ],
+    [
+        'name' => 'Alberto',
+        'surname' => 'Settimi',
+        'email' => 'alberto.settimi@example.com',
+        'password' => '12345678',
+    ],
+    [
+        'name' => 'Giorgio',
+        'surname' => 'Perfetti',
+        'email' => 'giorgio.perfetti@example.com',
+        'password' => '12345678',
+    ],
+    [
+        'name' => 'Sara',
+        'surname' => 'Costa',
+        'email' => 'sara.costa@example.com',
+        'password' => '12345678',
+    ],
     // e così via per gli altri utenti
 ];
 
