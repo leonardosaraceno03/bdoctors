a4ef927615b87f5f6d827d23f06b2661e76d5cb1
AndreaDeCeglia <deceglia89@gmail.com>
2023-02-22 18:13:18 +0100

    chiamata Axios in Home

--- resources/js/views/pages/HomePage.vue
+++ resources/js/views/pages/HomePage.vue
@@ -3,24 +3,43 @@
     <h1>
         Ciao sono la HOME PAGE
     </h1>
-    <!-- step2 - in questa pagina dobbiamo effettuare la chiamata Axios
-        per ciclare le Specializzazioni  -->
+    <Loader/>
   </div>
 </template>
 
 <script>
+import Loader from '../../components/Loader.vue'
 export default {
     name: 'HomePage',
-    components: {},
+    components: {
+        Loader,
+    },
     data(){
         return {
-
+            specializationsArray: [],
+            isLoading: false,
         }
     },
     mounted() {
-
+        this.getSpecializations();
     },
     methods: {
+        //step2 - in questa pagina dobbiamo effettuare la chiamata Axios
+        //per ciclare le Specializzazioni
+        getSpecializations(){
+            this.isLoading = true;
+            axios.get("http://localhost:8000/api/doctors")
+            .then((res) => {
+                console.log('questo è res.data', res.data);
+                this.specializationsArray = res.data;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .then(() => {
+                this.isLoading = false;
+            })
+        },
 
     }
 }
