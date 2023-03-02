7baadca71067314002241a74b49547c45c6440a7
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2023-03-02 16:43:22 +0000

    doctors & users

--- config/doctors.php
+++ config/doctors.php
@@ -1,105 +1,422 @@
 <?php
 return [
+
+    [
+        'address' => 'Via Roma, 1',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0123456789',
+        'performance' => 'Medicina generale, visite domiciliari, prevenzione',
+        'description' => 'Medico generico con anni di esperienza nel campo della salute',
+        'visibility' => true,
+        'user_id' => 1, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via Dante, 5',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0234567890',
+        'performance' => 'Cardiologia, Elettrocardiogramma, Visite specialistiche',
+        'description' => 'Esperto in cardiologia con anni di esperienza',
+        'visibility' => true,
+        'user_id' => 2, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via XX Settembre, 3',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0345678901',
+        'performance' => 'Ortopedia, Riabilitazione, Fisioterapia',
+        'description' => 'Specializzato in ortopedia e cure riabilitative',
+        'visibility' => true,
+        'user_id' => 3, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via Cavour, 10',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0456789012',
+        'performance' => 'Oculistica, Visite specialistiche, Chirurgia',
+        'description' => 'Medico specializzato in oftalmologia e chirurgia oculistica',
+        'visibility' => true,
+        'user_id' => 4, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via Garibaldi, 20',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0567890123',
+        'performance' => 'Otorinolaringoiatria, Visite specialistiche, Audiometria',
+        'description' => 'Esperto in otorinolaringoiatria con anni di esperienza nel campo della salute',
+        'visibility' => true,
+        'user_id' => 5, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Diaz, 15',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0678901234',
+        'performance' => 'Ginecologia, Visite specialistiche, Ecografia',
+        'description' => 'Specializzata in ginecologia e ostetricia, con anni di esperienza',
+        'visibility' => true,
+        'user_id' => 6, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via Milano, 15',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0789012345',
+        'performance' => 'Neurologia, Visite specialistiche, Elettroencefalogramma',
+        'description' => 'Medico specializzato in neurologia e cura della salute del sistema nervoso',
+        'visibility' => true,
+        'user_id' => 7, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via Torino, 30',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0890123456',
+        'performance' => 'Endocrinologia, Visite specialistiche, Esami del sangue',
+        'description' => 'Esperto in endocrinologia e cura della salute del sistema endocrino',
+        'visibility' => true,
+        'user_id' => 8, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via Verdi, 7',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0901234567',
+        'performance' => 'Ginecologia, Visite specialistiche, Ecografia',
+        'description' => 'Medico specializzato in ginecologia e cura della salute delle donne',
+        'visibility' => true,
+        'user_id' => 9, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via Garibaldi, 100',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0123456789',
+        'performance' => 'Allergologia, Visite specialistiche, Test allergici',
+        'description' => 'Specializzato in allergologia e cura delle allergie',
+        'visibility' => true,
+        'user_id' => 10, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Dante, 5',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0345678901',
+        'performance' => 'Oculistica, Visite specialistiche, Esami della vista',
+        'description' => 'Medico specializzato in oculistica e cura della salute degli occhi',
+        'visibility' => true,
+        'user_id' => 11, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via Garibaldi, 23',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0456789012',
+        'performance' => 'Ortopedia, Visite specialistiche, Radiografia',
+        'description' => `Specializzato in ortopedia e cura della salute dell'apparato muscolo-scheletrico`,
+        'visibility' => true,
+        'user_id' => 12, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via XX Settembre, 1',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0567890123',
+        'performance' => 'Psichiatria, Visite specialistiche, Terapia',
+        'description' => 'Medico specializzato in psichiatria e cura della salute mentale',
+        'visibility' => true,
+        'user_id' => 13, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via San Marco, 12',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0678901234',
+        'performance' => 'Cardiologia, Visite specialistiche, Elettrocardiogramma',
+        'description' => 'Specializzato in cardiologia e cura della salute del cuore',
+        'visibility' => true,
+        'user_id' => 14, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via della Libertà, 50',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0789012345',
+        'performance' => 'Chirurgia, Visite specialistiche, Interventi chirurgici',
+        'description' => 'Medico specializzato in chirurgia e interventi chirurgici',
+        'visibility' => true,
+        'user_id' => 15, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Garibaldi, 15',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '3284567890',
+        'performance' => 'Cardiologia, Diabetologia',
+        'description' => 'Esperto in malattie cardiache e diabete',
+        'visibility' => true,
+        'user_id' => 16, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Piazza del Popolo, 1',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '3331234567',
+        'performance' => 'Assistenza domiciliare, Infermieristica',
+        'description' => 'Esperta in assistenza domiciliare e infermieristica',
+        'visibility' => true,
+        'user_id' => 17,
+    ],
+
+    [
+        'address' => 'Corso Italia, 8',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '3456789012',
+        'performance' => 'Psicologia, Psicoterapia',
+        'description' => 'Esperta in psicologia e psicoterapia',
+        'visibility' => true,
+        'user_id' => 18,
+    ],
+
+    [
+        'address' => 'Via dei Mille, 23',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '3498765432',
+        'performance' => 'Odontoiatria',
+        'description' => 'Esperto in odontoiatria e cure per la salute dei denti',
+        'visibility' => true,
+        'user_id' => 19,
+    ],
+
+    [
+        'address' => 'Via Cesare Battisti, 10',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '3385678901',
+        'performance' => 'Fisioterapia',
+        'description' => 'Esperto in fisioterapia e riabilitazione',
+        'visibility' => true,
+        'user_id' => 20,
+    ],
+    [
+        'address' => 'Piazza del Popolo, 2',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '3456789012',
+        'performance' => 'Pediatria, Oculistica',
+        'description' => 'Specializzato in pediatria e oculistica per bambini',
+        'visibility' => true,
+        'user_id' => 21, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Garibaldi, 35',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '3481234567',
+        'performance' => 'Oftalmologia, Chirurgia oculare, Visite oculistiche',
+        'description' => 'Esperto in oftalmologia e cure per gli occhi',
+        'visibility' => true,
+        'user_id' => 23, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Roma, 7',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0112345678',
+        'performance' => 'Pediatria, Vaccinazioni, Visite pediatriche',
+        'description' => 'Specializzato in pediatria e cure per i bambini',
+        'visibility' => true,
+        'user_id' => 24, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Dante, 45',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0156789123',
+        'performance' => 'Ginecologia, Visite ginecologiche, Ecografie',
+        'description' => 'Esperto in ginecologia e cure per la salute femminile',
+        'visibility' => true,
+        'user_id' => 25, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Mazzini, 8',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0176543210',
+        'performance' => 'Ortopedia, Fisioterapia, Chirurgia ortopedica',
+        'description' => 'Esperto in ortopedia e cure per le patologie muscolo-scheletriche',
+        'visibility' => true,
+        'user_id' => 26, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Garibaldi, 75',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0198765432',
+        'performance' => 'Cardiologia, ECG, Ecocardiogramma',
+        'description' => 'Esperto in cardiologia e cure per la salute del cuore',
+        'visibility' => true,
+        'user_id' => 27, // ID dell'utente associato
+    ],
+
+    [
+        'address' => 'Via Veneto, 45',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '3456789123',
+        'performance' => 'Cardiologia, Medicina interna',
+        'description' => 'Specializzato in cardiologia e medicina interna',
+        'visibility' => true,
+        'user_id' => 28, // ID dell'utente associato
+    ],
+    [
+        'address' => 'Via Garibaldi, 73',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '4567891234',
+        'performance' => 'Ortopedia',
+        'description' => 'Esperto in ortopedia e cura delle malattie ossee',
+        'visibility' => true,
+        'user_id' => 29,
+    ],
+    [
+        'address' => 'Via della Libertà, 17',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '5678912345',
+        'performance' => 'Oculistica',
+        'description' => 'Esperto in oculistica e cura della vista',
+        'visibility' => true,
+        'user_id' => 30,
+    ],
+
+    [
+        'address' => 'Via Roma, 89',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '6789123456',
+        'performance' => 'Endocrinologia',
+        'description' => 'Specializzato in endocrinologia e cura delle disfunzioni ormonali',
+        'visibility' => true,
+        'user_id' => 31,
+    ],
+
+    [
+        'address' => 'Via Dante, 21',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '7891234567',
+        'performance' => 'Pneumologia',
+        'description' => 'Esperto in pneumologia e cura delle malattie respiratorie',
+        'visibility' => true,
+        'user_id' => 32,
+    ],
+
     [
         'address' => 'Via Roma, 123',
         'cv' => 'cv/cv-default.jpg',
         'avatar' => 'avatars/default-avatar.png',
         'telephone' => '0123456789',
-        'performance' => 'Eccellente',
-        'description' => 'Lorem ipsum dolor sit amet...',
+        'performance' => 'Pediatria,Allergologia',
+        'description' => 'Specializzato in pediatria e allergologia pediatrica',
         'visibility' => true,
-        'user_id' => 1, // ID dell'utente associato
-    ],
-    [
-        'address' => 'Via Milano, 456',
-        'cv' => 'cv/cv-default.jpg',
-        'avatar' => 'avatars/default-avatar.png',
-        'telephone' => '9876543210',
-        'performance' => 'Buona',
-        'description' => 'Lorem ipsum dolor sit amet...',
-        'visibility' => true,
-        'user_id' => 2, // ID dell'utente associato
-    ],
-    [
-        'address' => 'Via Milano, 132',
-        'cv' => 'cv/cv-default.jpg',
-        'avatar' => 'avatars/default-avatar.png',
-        'telephone' => '9876543210',
-        'performance' => 'Ottimo',
-        'description' => 'Lorem ipsum dolor sit amet...',
-        'visibility' => true,
-        'user_id' => 3, // ID dell'utente associato
-    ],
-    [
-        'address' => 'Via Dante, 234',
-        'cv' => 'cv/cv-default.jpg',
-        'avatar' => 'avatars/default-avatar.png',
-        'telephone' => '3334445555',
-        'performance' => 'Molto buono',
-        'description' => 'Lorem ipsum dolor sit amet...',
-        'visibility' => true,
-        'user_id' => 4, // ID dell'utente associato
-    ],
-    [
-        'address' => 'Piazza del Popolo, 567',
-        'cv' => 'cv/cv-default.jpg',
-        'avatar' => 'avatars/default-avatar.png',
-        'telephone' => '9998887777',
-        'performance' => 'Eccellente',
-        'description' => 'Lorem ipsum dolor sit amet...',
-        'visibility' => true,
-        'user_id' => 5, // ID dell'utente associato
-    ],
-    [
-        'address' => 'Via Garibaldi, 890',
-        'cv' => 'cv/cv-default.jpg',
-        'avatar' => 'avatars/default-avatar.png',
-        'telephone' => '1112223333',
-        'performance' => 'Discreto',
-        'description' => 'Lorem ipsum dolor sit amet...',
-        'visibility' => true,
-        'user_id' => 6, // ID dell'utente associato
-    ],
-    [
-        'address' => 'Via delle Rose, 123',
-        'cv' => 'cv/cv-default.jpg',
-        'avatar' => 'avatars/default-avatar.png',
-        'telephone' => '4445556666',
-        'performance' => 'Molto buono',
-        'description' => 'Lorem ipsum dolor sit amet...',
-        'visibility' => true,
-        'user_id' => 7, // ID dell'utente associato
-    ],
-    [
-        'address' => 'Via dei Mille, 456',
-        'cv' => 'cv/cv-default.jpg',
-        'avatar' => 'avatars/default-avatar.png',
-        'telephone' => '7778889999',
-        'performance' => 'Ottimo',
-        'description' => 'Lorem ipsum dolor sit amet...',
-        'visibility' => true,
-        'user_id' => 8, // ID dell'utente associato
-    ],
-    [
-        'address' => 'Via dei Tigli, 234',
-        'cv' => 'cv/cv-default.jpg',
-        'avatar' => 'avatars/default-avatar.png',
-        'telephone' => '4443332222',
-        'performance' => 'Eccellente',
-        'description' => 'Lorem ipsum dolor sit amet...',
-        'visibility' => true,
-        'user_id' => 9, // ID dell'utente associato
-    ],
-    [
-        'address' => 'Via del Corso, 567',
-        'cv' => 'cv/cv-default.jpg',
-        'avatar' => 'avatars/default-avatar.png',
-        'telephone' => '8887776666',
-        'performance' => 'Molto buono',
-        'description' => 'Lorem ipsum dolor sit amet...',
-        'visibility' => true,
-        'user_id' => 10, // ID dell'utente associato
+        'user_id' => 33,
     ],
 
-    // e così via per gli altri dottori
+    [
+        'address' => 'Via Dante, 50',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '3456789012',
+        'performance' => 'Oftalmologia',
+        'description' => 'Esperto in oftalmologia e malattie degli occhi',
+        'visibility' => true,
+        'user_id' => 34,
+    ],
+
+    [
+        'address' => 'Via Verdi, 8',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '7890123456',
+        'performance' => 'Chirurgia Plastica',
+        'description' => 'Specializzato in chirurgia plastica e ricostruttiva',
+        'visibility' => true,
+        'user_id' => 35,
+    ],
+
+    [
+        'address' => 'Via Milano, 32',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '5678901234',
+        'performance' => 'Ortopedia, Traumatologia',
+        'description' => 'Esperto in ortopedia e traumatologia',
+        'visibility' => true,
+        'user_id' => 36,
+    ],
+
+    [
+        'address' => 'Via Garibaldi, 5',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '9012345678',
+        'performance' => 'Cardiologia, Pneumologia',
+        'description' => 'Specializzato in cardiologia e pneumologia',
+        'visibility' => true,
+        'user_id' => 37,
+    ],
+
+    [
+        'address' => 'Via Garibaldi, 15',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0123456789',
+        'performance' => 'Oculistica,Chirurgia refrattiva,Chirurgia della cataratta',
+        'description' => 'Esperto in oculistica e interventi chirurgici per la vista',
+        'visibility' => true,
+        'user_id' => 38,
+    ],
+
+    [
+        'address' => 'Via Dante Alighieri, 7',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0123456789',
+        'performance' => 'Cardiologia,Cardiochirurgia',
+        'description' => 'Specializzato in cardiologia e chirurgia cardiovascolare',
+        'visibility' => true,
+        'user_id' => 39,
+    ],
+
+    [
+        'address' => 'Via Garibaldi, 15',
+        'cv' => 'cv/cv-default.jpg',
+        'avatar' => 'avatars/default-avatar.png',
+        'telephone' => '0123456789',
+        'performance' => 'Ortopedia,Traumatologia,Chirurgia della mano',
+        'description' => 'Esperto in ortopedia, traumatologia e chirurgia della mano',
+        'visibility' => true,
+        'user_id' => 40,
+    ],
+
 ];
