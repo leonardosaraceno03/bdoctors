c102593da7779b8647e0e9c02ac9066f2209742f
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2023-03-03 14:10:35 +0000

    navbar homepage

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,40 +1,93 @@
 <template>
-    <div>
-      <nav class="navbar navbar-expand-lg bg-light d-flex justify-content-between align-items-center container-fluid px-5">
+  <div>
+    <nav
+      class="navbar navbar-expand-lg bg-light d-flex justify-content-between align-items-center container-fluid px-5"
+    >
+      <div
+        class="container-fluid d-flex justify-content-between align-items-center px-4"
+      >
         <div>
-            <a class="navbar-brand" href="/">BDoctors</a>
-            <a class="navbar-brand" href="/doctors">FilterPage</a>
+          <router-link class="navbar-brand" :to="`/`"> BDoctors </router-link>
+
+          <router-link class="navbar-brand" :to="`/doctors`">
+            Cerca il tuo Dottore
+          </router-link>
         </div>
-        <div class="">
 
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav">
+        <div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
 
-                      <li class="nav-item">
-                          <a href="/login" class="nav-link">Accedi</a>
-                      </li>
-                      <li class="nav-item">
-                          <a href="/register" class="nav-link">Registrati</a>
-                      </li>
-
-                  </ul>
-              </div>
+        <div class="dropdown" v-if="authUser == null">
+          <div class="navbar-nav">
+            <a href="/login" class="nav-link">Accedi</a>
+            <a href="/register" class="nav-link">Registrati</a>
           </div>
-        </nav>
+        </div>
+        <div class="dropdown" v-else>
+          <a
+            class="btn dropdown-toggle"
+            href="#"
+            role="button"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <i class="fas fa-user"></i>
+          </a>
 
-    </div>
-
-  </template>
+          <div class="dropdown-menu" style="left: auto; right: 0">
+            <a class="dropdown-item text-muted" href="/admin/doctors/index"
+              >Profilo</a
+            >
+            <a class="dropdown-item text-muted" href="/admin/doctors"
+              >Dashboard</a
+            >
+            <a
+              class="dropdown-item text-muted"
+              href="#"
+              @click.prevent="logoutUser"
+              >Logout</a
+            >
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
 
   <script>
-  export default {
-      name: 'Header',
-  }
-  </script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      authUser: window.authUser,
+    };
+  },
+  created() {
+    console.log(this.authUser);
+  },
 
-  <style>
-
-  </style>
+  methods: {
+    logoutUser() {
+      axios
+        .post("/logout")
+        .then((response) => {
+          window.location = "/";
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,22 +8,35 @@
 
 
     {{-- Css --}}
-    <link rel=“stylesheet” href="{{asset ('css/app.css')}}">
+    <link rel=“stylesheet” href="{{ asset('css/app.css') }}">
     <script src="https://kit.fontawesome.com/20d85c6a60.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     {{-- Title --}}
     <title>BDoctors</title>
 
 </head>
+
 <body>
     <div id="root">
-
+        @if (Auth::check())
+            <script>
+                window.authUser = {!! json_encode(Auth::user()) !!};
+            </script>
+        @else
+            <script>
+                window.authUser = null;
+            </script>
+        @endif
     </div>
 
 
 
     {{-- inserisco lo script relativo alla porzione pubblica --}}
-    <script src="{{asset('js/app.js')}}" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
